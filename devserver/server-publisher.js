c283107a0aa03a81a8e5fa71ad2101107052028e
Anuj Jain <jainanuj0812@gmail.com>
2018-03-22 02:54:33 +0530

    Added approach page

--- devserver/server-publisher.js
+++ devserver/server-publisher.js
@@ -38,21 +38,21 @@ for (let ccy in startPrices) {
 exports.start = function(stompUrl) {
   var client = Stomp.overWS(stompUrl);
   client.connect({}, function() {
-    util.log("Fake FX data service connected: serving price updates on stomp topic /fx/prices")
+    util.log("Fake FX data service connected: serving price updates on stomp topic /fx/prices");
     setInterval(function tick() {
         for (let i = 0; i < Math.random() * 5; i++) {
-          const randomCurrency = currencies[Math.floor(Math.random() * currencies.length)]
-          const mid = internal[randomCurrency]
-          const spread = Math.random() * 0.05
-          const diff = (Math.random() * 0.08 - 0.04) * mid
-          const newMid = (mid + diff)
-          const bid = newMid - newMid * (spread / 2)
-          const ask = newMid + newMid * (spread / 2)
-          const data = publicData[randomCurrency]
-          data.lastChangeBid = bid - data.bestBid
-          data.lastChangeAsk = ask - data.bestAsk
-          data.bestBid = bid
-          data.bestAsk = ask
+          const randomCurrency = currencies[Math.floor(Math.random() * currencies.length)];
+          const mid = internal[randomCurrency];
+          const spread = Math.random() * 0.05;
+          const diff = (Math.random() * 0.08 - 0.04) * mid;
+          const newMid = (mid + diff);
+          const bid = newMid - newMid * (spread / 2);
+          const ask = newMid + newMid * (spread / 2);
+          const data = publicData[randomCurrency];
+          data.lastChangeBid = bid - data.bestBid;
+          data.lastChangeAsk = ask - data.bestAsk;
+          data.bestBid = bid;
+          data.bestAsk = ask;
 
           client.send("/fx/prices", {priority: 9}, JSON.stringify(data))
         }
